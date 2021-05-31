51cf961ab0616a7d189109febb70b23de1909b23
Edson Batista <edson.batista@catho.com>
2021-05-31 14:54:56 -0300

    feat: use EmailValidator into SingupController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,16 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
 import { BadRequest } from '../helpers/http-helper';
 import { ControllerHttp } from '../protocols/controller-http';
+import { EmailValidator } from '../protocols/email-validator';
+import { InvalidParamError } from '../errors/invalid-param-error';
 
 export class SignUpController implements ControllerHttp {
+  private readonly emailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 
@@ -11,6 +19,12 @@ export class SignUpController implements ControllerHttp {
       if (!httpRequest.body[field]) {
         return BadRequest(new MissingParamError(field));
       }
+    }
+
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+
+    if (!isValidEmail) {
+      return BadRequest(new InvalidParamError('email'));
     }
 
     return {
