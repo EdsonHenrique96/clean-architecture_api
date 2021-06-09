e3e7562aa6db14b6c7dcb4d95846d076e3fcf605
Edson Batista <edson.batista@catho.com>
2021-06-09 02:33:39 -0300

    test: move sut creation to a helper factory function

--- src/data/db-add-account.spec.ts
+++ src/data/db-add-account.spec.ts
@@ -1,17 +1,30 @@
 import { DbAddAccount } from './db-add-account';
 import { Encrypter } from './protocols/encrypter';
 
-class EncrypterStub implements Encrypter {
-  async encrypt(data: string): Promise<string> { //eslint-disable-line
-    return new Promise((resolve) => resolve('encrypted_password'));
-  }
+interface MakeSutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
 }
+
+const makeSut = (): MakeSutTypes => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(data: string): Promise<string> { //eslint-disable-line
+      return new Promise((resolve) => resolve('encrypted_password'));
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbAddAccount(encrypterStub);
+
+  return {
+    sut,
+    encrypterStub,
+  };
+};
 
 describe('UseCase DbAddAccount', () => {
   test('Should call Encrypter with correct password', async () => {
-    const encrypterStub = new EncrypterStub();
-
-    const sut = new DbAddAccount(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
 
     const dataAccount = {
       name: 'valid_name',
@@ -26,8 +39,7 @@ describe('UseCase DbAddAccount', () => {
   });
 
   test('Should throw when encrypter throws', async () => {
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbAddAccount(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
 
     jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(Promise.reject(new Error()));
 
